e48089c6a2e5e50b9912149ebc59ed08865176b8
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-15 18:40:50 -0500

    working with core components

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
 export default function App() {
   console.log("App executed");
-  let x =1;
   return (
     <View style={styles.container}>
       <Text>Hello, I'm Sean and I'm working on a mobile app!</Text>
       <Text>Mike Sparkman!!!!!</Text>
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
