a5898b5211521ce17b19f298f916151fdb758452
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-14 20:49:05 -0500

    created native app with expo and added readme

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  let x =1;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, I'm Sean and I'm working on a mobile app!</Text>
+      <Text>Mike Sparkman!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
