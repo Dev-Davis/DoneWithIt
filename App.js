844c50836b901e9d28f97f5f71dce6e575794de1
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-18 19:42:13 -0500

    Flexbox - flexWrap and alignContent

--- App.js
+++ App.js
@@ -8,29 +8,35 @@ export default function App() {
       flex: 1,
       justifyContent: 'center',
       flexDirection: 'row', // main axis is row or column
-      alignItems: 'center' // secondary axis
+      alignItems: 'center', // secondary axis
+      alignContent: 'center',
+      flexWrap: 'wrap',
     }}>
       <View style={{
         backgroundColor: 'dodgerblue',
         width: 100,
-        height: 300
-      }}>
-        
-      </View>
+        height: 100
+      }} />
       <View style={{
         backgroundColor: 'gold',
         width: 100,
-        height: 200
-      }}>
-        
-      </View>
+        height: 100
+      }} />
       <View style={{
         backgroundColor: 'tomato',
         width: 100,
         height: 100
-      }}>
-        
-      </View>
+      }} />
+      <View style={{
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'greenyellow',
+        width: 100,
+        height: 100
+      }} />
     </View>
   );
 }
