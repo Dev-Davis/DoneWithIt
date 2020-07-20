e12e8601af7109b71318530678f45c71e194d037
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-20 18:34:12 -0500

    borders

--- App.js
+++ App.js
@@ -4,5 +4,25 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+      <View
+        style={{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderColor: 'royalblue',
+        borderRadius: 50,
+      }}
+      >
+      </View>
+    </View>
+  );
 }
