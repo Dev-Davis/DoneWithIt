e257be8400b16758db8c08e57cc3550ac3771efe
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-22 19:26:30 -0500

    Platform specific code - refacoring between platforms

--- App.js
+++ App.js
@@ -16,17 +16,7 @@ export default function App() {
         alignItems: 'center',
       }}
       >
-      <View
-        style={{
-        backgroundColor: 'dodgerblue',
-        width: 100,
-        height: 100,
-        shadowColor: 'grey',
-        shadowOffset: { width: 10, height: 10 },
-        shadowOpacity: 1,
-      }}
-      >
-      </View>
+      
       <View style={{
         flex: 1,
         justifyContent: 'center',
