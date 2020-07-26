78b6e81e76c596ec72373c6890ca5870a46b7b54
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-26 18:35:37 -0500

    Building the component card

---
+++ app/components/Card.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+import Colors from '../config/colors'
+import AppText from './AppText/AppText'
+
+export default function Card({ title, subTitle, pic }) {
+    return (
+        <View style={styles.card}>
+            <Image style={styles.image} source={pic} />
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title}>{title}</AppText>
+                <AppText style={styles.subTitle}>{subTitle}</AppText>
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 15,
+        backgroundColor: Colors.white,
+        marginBottom: 20,
+        overflow: 'hidden',
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    detailsContainer: {
+        padding: 20,
+    },
+    subTitle: {
+        color: Colors.secondary,
+        fontWeight: 'bold',
+        fontSize: 33,
+    },
+    title: {
+        marginBottom: 7,
+    },
+})
