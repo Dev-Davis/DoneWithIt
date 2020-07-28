f5e3c759d63f3163a00d24a44f66487dba6cb6ca
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-28 17:56:25 -0500

    Improving the View Image Screen

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { View } from 'react-native';
 
 import Card from "./app/components/Card";
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <ListingDetailsScreen />
+    <ViewImageScreen />
   )
 }
