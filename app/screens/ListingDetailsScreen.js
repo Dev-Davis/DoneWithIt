93acb432aae7729d180d6fd8d507ca7e98f01a45
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-07-27 20:48:49 -0500

    Building the ListItem Component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { View, Image, StyleSheet } from 'react-native'
+
+import ListItem from '../components/ListItem'
 import AppText from '../components/AppText/AppText'
 import  Colors  from '../config/colors'
 
@@ -10,6 +12,13 @@ export default function ListingDetailsScreen() {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale</AppText>
                 <AppText style={styles.price}>$100</AppText>
+                <View style={styles.userContainer}>
+                    <ListItem
+                        image={require('../assets/selfie.jpg')}
+                        title="Dott The Dev"
+                        subTitle="5 Listings"
+                    />
+                </View>
             </View>
         </View>
     )
@@ -32,5 +41,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: '500',
+    },
+    userContainer: {
+        marginVertical: 40,
     },
 })
