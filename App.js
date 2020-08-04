2e14c3bd8a2045309aa4054af049afbbceb21ef9
Sean Davis <sean.dev.davis@gmail.com>
2020-08-04 17:37:39 -0500

    Deleting an item

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 import Card from "./app/components/Card";
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return (
-    <MessagesScreen />
-  )
+  return <MessagesScreen />;
 }
