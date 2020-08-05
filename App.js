740905537e906f1e12109cbc0e748056c1ca44c4
Sean Davis <sean.dev.davis@gmail.com>
2020-08-04 20:00:55 -0500

    Building the Icon Component and Extending the ListItem Component

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      <ListItem title="My Title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
