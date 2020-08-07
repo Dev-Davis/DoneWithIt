c07f472ca7aaa3f2e4316ac305e2f7a7de428931
Sean Davis <sean.dev.davis@gmail.com>
2020-08-07 16:31:39 -0500

    Building the Listings Screen

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <AccountScreen />;
+  return <ListingsScreen />;
 }
