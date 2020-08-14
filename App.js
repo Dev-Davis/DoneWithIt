6cd97a257e1f8c1df69f01e2925634bf66a30508
Sean Davis <sean.dev.davis@gmail.com>
2020-08-14 17:21:48 -0500

    Showing Picker Items

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
 import React, { useState } from "react";
-import { View, TextInput, Text, Switch } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
-import AppText from "./app/components/AppText";
 import AppPicker from './app/components/AppPicker'
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Electronics", value: 3},
+];
+
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppPicker icon="apps" placeholder="Category" />
+      <AppPicker items={categories} icon="apps" placeholder="Category" />
       <AppTextInput  icon="email" placeholder="Email" />
     </Screen>
   );   
